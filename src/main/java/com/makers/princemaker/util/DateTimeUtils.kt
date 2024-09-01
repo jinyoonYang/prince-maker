57a9da9e5cab1729f66bc7a139da3ad0430f10d1
jinyoonYang <didwlsdbs123@naver.com>
2024-09-01 17:58:06 +0900

    1. Utils 클래스 코틀린 변환

--- src/main/java/com/makers/princemaker/util/DateTimeUtils.kt
+++ src/main/java/com/makers/princemaker/util/DateTimeUtils.kt
@@ -1,15 +1,25 @@
-package com.makers.princemaker.util;
+package com.makers.princemaker.util
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
-import lombok.experimental.UtilityClass;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-@UtilityClass
-public class DateTimeUtils {
-    public static String getLocalDateTimeString(LocalDateTime localDateTime) {
-        return localDateTime.format(DateTimeFormatter
-                .ofPattern("yyyy-MM-dd 탄생"));
+//버전 1 (object 사용)
+/*
+//object - 싱글톤 패턴으로 생성됨
+object DateTimeUtils {
+    @JvmStatic //해당 함수를 static으로 생성해줌
+    fun getLocalDateTimeString(localDateTime: LocalDateTime): String {
+        return localDateTime.format(
+            DateTimeFormatter
+                .ofPattern("yyyy-MM-dd 탄생")
+        )
     }
+}*/
+
+//버전 2 (글로벌 함수 - top 레벨)
+fun getLocalDateTimeString(localDateTime: LocalDateTime): String {
+    return localDateTime.format(
+        DateTimeFormatter
+            .ofPattern("yyyy-MM-dd 탄생")
+    )
 }
